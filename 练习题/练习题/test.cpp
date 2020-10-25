7793d2027cc72ba4ba5a4328f677ec823b3f570b
Coderxingchild <1272209351@qq.com>
2020-10-25 12:06:13 +0800

    类和对象练习题+内存管理

---
+++ 练习题/练习题/test.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+using namespace std;
+
+//内存管理
+
+//malloc \ calloc \ realloc
+
+class Date {
+	int _year = 1900;
+	int _mont = 1;
+	int _day = 1;
+};
+void test()
+{
+	Date *pd = (Date*)malloc(sizeof(Date));
+}
+int main()
+{
+	test();  //随机值：malloc 只开辟空间不进行初始化
+	return 0;
+}
+
+
+
+
+//计算给定日期是这一年的第多少天
+//int main()
+//{
+//	static int days[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+//	int year, month, day;
+//	while (cin >> year >> month >> day) {
+//		int ret = 0;
+//		for (int i = 1; i < month; ++i) {
+//			ret += days[i];
+//		}
+//		ret += day;
+//		if (month > 2) {
+//			if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//				++ret;
+//			}
+//			cout << ret << endl;
+//		}
+//	}
+//}
+
+
+
+//1+2+3+4+..............+n
+//class Solution {
+//public:
+//	class Sum {
+//	public:
+//		Sum() {
+//			_sum = 0;    //重置
+//			_i = 1;
+//			_sum += _i;
+//			++_i;
+//		}
+//	};
+//	int Sum_Solution(int n) {
+//		Sum arr[n];
+//		return _sum;
+//	}
+//private:
+//	static int _sum;
+//	static int _i;
+//};
+////静态成员在类外初始化
+//int Solution::_sum = 0;
+//int Solution::_i = 1;
