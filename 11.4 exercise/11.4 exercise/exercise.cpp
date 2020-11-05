fd9c33e42feb7af9defbefe682426b2f50fe8687
Coderxingchild <1272209351@qq.com>
2020-11-05 11:06:54 +0800

    insert、push_back、pop_back、swap、replace、erase

--- 11.4 exercise/11.4 exercise/exercise.cpp
+++ 11.4 exercise/11.4 exercise/exercise.cpp
@@ -3,6 +3,68 @@
 using namespace std;
 
 
+
+
+
+
+
+
+// push_back()  尾插
+//pop_back()   尾删
+
+//insert  插入接口
+//erase()   删除接口
+//assign  赋值
+//replace 替换
+
+
+//void test()
+//{
+//	string s = "12342354";
+//	string s2 = "423";
+//	size_t pos = s.find(s2);
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
+
+//void test()
+//{
+//	string s = "123";
+//	string s2 = "abc";
+//	s.replace(1, 1, s2);     // 1abc3
+//	swap(s, s2);
+//	s.swap(s2);
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
+
+//void test()
+//{
+//	string s = "12345";
+//	string::const_reverse_iterator cit = s.crbegin();
+//	while (cit != s.crend)
+//	{
+//		//*cit = 'a';   // 只读不可修改
+//
+//		cout << *cit << endl;
+//		++cit;
+//	}
+//	cout << endl;
+//}
+
+
+
 //void test()
 //{
 //	string s ("123");
@@ -55,6 +117,9 @@ using namespace std;
 //}
 
 
+
+//  string
+
 //void test()
 //{
 //	string str1 = "13356456";
@@ -82,18 +147,99 @@ using namespace std;
 
 
 
-void test()
-{
+//void test()
+//{
+//	//string str = "12345";
+//	//int i = 0;
+//	//for (; i <= str.size();++i) {
+//	//	cout << str[i] << " ";
+//	//}
+//	//cout << endl;
+//
+//	//迭代器
+//	//string str1 = "12345";
+//	//string::iterator it = str1.begin();
+//	//while (it != str1.end()) {
+//	//	*it = 'a';
+//	//	cout << *it << " ";
+//	//	++it;
+//	//}
+//	//cout << endl;
+//
+//	//const string str2 = "12345";
+//	//string::const_iterator it1 = str2.begin();
+//	//while (it != str2.end()) {
+//	//	// *it = 'a';
+//	//	cout << *it << " ";
+//	//	++it;
+//	//}
+//	//cout << endl;
+//
+//
+//	//const string str3 = "12345";
+//	//string::const_iterator it3 = str3.cbegin();
+//	//while (it3 != str3.cend()) {
+//	//	
+//	//	//*it = 'a';
+//	//	
+//	//	cout << *it3 << " ";
+//	//	++it3;
+//	//}
+//	//cout << endl;
+//
+//
+//	//反向迭代器
+//	//string str4 = "12345";
+//	//string::reverse_iterator it4 = str4.rbegin();
+//	//while (it4 != str4.rend()) {
+//	//	cout << *it4 << " ";
+//	//	++it4;
+//	//}
+//	//cout << endl;
+//
+//
+//	/*string str = "123456";
+//	string::iterator it = str.begin();
+//	while (it != str.end()) {
+//		cout << *it << " " << endl;
+//		++it;
+//	}*/
+//}
 
-}
-
-int main()
-{
-	test();
-	return 0;
-}
 
 
+//范围 for
+
+//void test()
+//{
+//	string s = "13456";
+//	for (char ch : s) {
+//		cout << ch << " ";
+//	}
+//	cout << endl;
+//}
+
+
+//void test()
+//{
+//	string s = "1224235";
+//	/*for (const auto& ch : s) {*/
+//	for(auto& ch:s){
+//		ch = 'a';
+//
+//		cout << ch << " ";
+//	}
+//	cout << endl;
+//}
+//
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+
+//模板
 
 //template<typename T1, typename T2>
 //T1 Add(const T1& a, const T2& b)
@@ -112,7 +258,7 @@ int main()
 //	Add(a, b);
 //	Add(a, c);
 //
-//	Add<int>(a, c);
+//	Add<int>(a, c);       //显式实例化
 //	Add<double>(a, c);
 //
 //}
