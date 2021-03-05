3b896a4f202b41a88a190a2706ef356e8a18e765
Coderxingchild <1272209351@qq.com>
2021-03-05 14:53:39 +0800

    类与对象

--- class_lei_1/class_lei_1/Date.cpp
+++ class_lei_1/class_lei_1/Date.cpp
@@ -1,162 +1,162 @@
-#include<iostream>
-#include<stdlib.h>
-
-using namespace std;
-
-class Date {
-public:
-	Date(int y = 1,int m = 1,int d = 1)
-	{
-		//判断日期是否有效
-		if (y <= 0 || m <= 0 || m > 12 || d <= 0 || d > getDay(y, m))
-		{
-			//日期无效
-			_y = 1; _m = 1; _d = 1;
-			cout << "日期无效，设为默认值：1-1-1" << endl;
-		}
-		else {
-			_y = y;
-			_m = m;
-			_d = d;
-		}	
-	}
-	int getDay(int y, int m)
-	{
-		static int days[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
-		int day = days[m];
-		//判断闰年的二月
-		if (m == 2 && ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0))) {
-			day+=1;
-		}
-		return day;
-	}
-	//日期运算
-	Date& operator+=(int day)
-	{
-		if (day < 0)   //加一个负数 === 减一个正数
-			return *this -= -day;
-		this->_d += day;    //先相加天数
-		while(this->_d > getDay(_y, _m)) {
-			_d -= getDay(_y, _m);
-			++_m;
-			if (_m == 13) {
-				_m = 1;
-				++_y;
-			}
-		}
-		return *this;
-	}
-	Date operator+(int day)
-	{
-		//Date d = *this;
-		//Date d(*this);
-		Date d;  //拷贝 this 指针内容
-		d._y = _y;
-		d._m = _m;
-		d._d = _d;
-		return d += day;
-	}
-	Date& operator-=(int day)
-	{
-		if (day < 0) {  //减去一个负数====加一个正数
-			return *this += -day;
-		}
-		else {    //减一个正数
-			_d -= day;
-			while (_d <= 0) {
-				--_m;
-				if (0 == _m) {
-					--_y;
-					_m = 12;
-				}
-				_d += getDay(_y, _m);
-			}
-		}
-		return *this;
-	}
-	Date operator-(int day)
-	{
-		Date d = *this;
-		return d -= day;
-	}
-	Date& operator++()
-	{        //前置 ++
-		return *this += 1;
-	}
-	Date operator++(int)  //int 参数只是为了与前置 ++ 进行区分形成重载，实际无用
-	{
-		//后置 ++
-		Date tmp = *this;
-		*this += 1;
-		return tmp;
-	}
-	Date& operator--()
-	{
-		//前置 -- ，返回修改后值
-		return *this -= 1;
-	}
-	Date operator--(int)
-	{
-		//后置 --，返回原值
-		Date copy = *this;
-		*this -= 1;
-		return copy;
-	}
-	bool operator==(const Date& d)
-	{  //判等
-		return _y == d._y && _m == d._m && _d == d._d;
-	}
-	bool operator>(const Date& d)
-	{    // 大于
-		if (_y > d._y)
-			return true;
-		else if (_y == d._y) {
-			if (_m > d._m)
-				return true;
-			else if (_m == d._m) {
-				if (_d > d._d)
-					return true;
-			}
-		}
-		return false;
-	}
-	bool operator!=(const Date& d)
-	{
-		return !(*this == d);
-	}
-	bool operator>=(const Date& d)
-	{
-		return (*this > d) || (*this == d);
-	}
-	bool operator<=(const Date& d)
-	{
-		return !(*this > d);
-	}
-	bool operator<(const Date& d)
-	{
-		return !(*this >= d);
-	}
-	//两个日期相减
-	int operator-(const Date& d)
-	{
-		//小日期自加多少天等于大日期
-		Date max = *this;    /* Date max(*this)*/
-		Date min = d;
-		int flag = 1;
-		if (max < min) {
-			max = d;
-			min = *this;
-			flag = -1;
-		}
-		int day = 0;
-		while (min < max) {
-			++min;
-			++day;
-		}
-		return flag * day;
-	}
-private:
-	int _y;
-	int _m;
-	int _d;
-};
+//#include<iostream>
+//#include<stdlib.h>
+//
+//using namespace std;
+//
+//class Date {
+//public:
+//	Date(int y = 1,int m = 1,int d = 1)
+//	{
+//		//判断日期是否有效
+//		if (y <= 0 || m <= 0 || m > 12 || d <= 0 || d > getDay(y, m))
+//		{
+//			//日期无效
+//			_y = 1; _m = 1; _d = 1;
+//			cout << "日期无效，设为默认值：1-1-1" << endl;
+//		}
+//		else {
+//			_y = y;
+//			_m = m;
+//			_d = d;
+//		}	
+//	}
+//	int getDay(int y, int m)
+//	{
+//		static int days[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+//		int day = days[m];
+//		//判断闰年的二月
+//		if (m == 2 && ((y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0))) {
+//			day+=1;
+//		}
+//		return day;
+//	}
+//	//日期运算
+//	Date& operator+=(int day)
+//	{
+//		if (day < 0)   //加一个负数 === 减一个正数
+//			return *this -= -day;
+//		this->_d += day;    //先相加天数
+//		while(this->_d > getDay(_y, _m)) {
+//			_d -= getDay(_y, _m);
+//			++_m;
+//			if (_m == 13) {
+//				_m = 1;
+//				++_y;
+//			}
+//		}
+//		return *this;
+//	}
+//	Date operator+(int day)
+//	{
+//		//Date d = *this;
+//		//Date d(*this);
+//		Date d;  //拷贝 this 指针内容
+//		d._y = _y;
+//		d._m = _m;
+//		d._d = _d;
+//		return d += day;
+//	}
+//	Date& operator-=(int day)
+//	{
+//		if (day < 0) {  //减去一个负数====加一个正数
+//			return *this += -day;
+//		}
+//		else {    //减一个正数
+//			_d -= day;
+//			while (_d <= 0) {
+//				--_m;
+//				if (0 == _m) {
+//					--_y;
+//					_m = 12;
+//				}
+//				_d += getDay(_y, _m);
+//			}
+//		}
+//		return *this;
+//	}
+//	Date operator-(int day)
+//	{
+//		Date d = *this;
+//		return d -= day;
+//	}
+//	Date& operator++()
+//	{        //前置 ++
+//		return *this += 1;
+//	}
+//	Date operator++(int)  //int 参数只是为了与前置 ++ 进行区分形成重载，实际无用
+//	{
+//		//后置 ++
+//		Date tmp = *this;
+//		*this += 1;
+//		return tmp;
+//	}
+//	Date& operator--()
+//	{
+//		//前置 -- ，返回修改后值
+//		return *this -= 1;
+//	}
+//	Date operator--(int)
+//	{
+//		//后置 --，返回原值
+//		Date copy = *this;
+//		*this -= 1;
+//		return copy;
+//	}
+//	bool operator==(const Date& d)
+//	{  //判等
+//		return _y == d._y && _m == d._m && _d == d._d;
+//	}
+//	bool operator>(const Date& d)
+//	{    // 大于
+//		if (_y > d._y)
+//			return true;
+//		else if (_y == d._y) {
+//			if (_m > d._m)
+//				return true;
+//			else if (_m == d._m) {
+//				if (_d > d._d)
+//					return true;
+//			}
+//		}
+//		return false;
+//	}
+//	bool operator!=(const Date& d)
+//	{
+//		return !(*this == d);
+//	}
+//	bool operator>=(const Date& d)
+//	{
+//		return (*this > d) || (*this == d);
+//	}
+//	bool operator<=(const Date& d)
+//	{
+//		return !(*this > d);
+//	}
+//	bool operator<(const Date& d)
+//	{
+//		return !(*this >= d);
+//	}
+//	//两个日期相减
+//	int operator-(const Date& d)
+//	{
+//		//小日期自加多少天等于大日期
+//		Date max = *this;    /* Date max(*this)*/
+//		Date min = d;
+//		int flag = 1;
+//		if (max < min) {
+//			max = d;
+//			min = *this;
+//			flag = -1;
+//		}
+//		int day = 0;
+//		while (min < max) {
+//			++min;
+//			++day;
+//		}
+//		return flag * day;
+//	}
+//private:
+//	int _y;
+//	int _m;
+//	int _d;
+//};
--- class_lei_1/class_lei_1/test_lei.cpp
+++ class_lei_1/class_lei_1/test_lei.cpp
@@ -5,38 +5,119 @@ using namespace std;
 // explicit 关键字防止单参构造隐式类型转换
 
 //static 成员变量必须在类外初始化
-//static 成员是所有对象共享的
+//static 成员是所有对象共享的(变量地址相同)
 //访问形式：  1. 对象 . static成员    2.类型 :: static 成员
 // static 成员函数不包含 this 指针
 //普通函数可以调用 static 成员函数
 
 
 
+class Date {
+public:
+	//友元函数的声明可以放在类内任何地方
+	friend void operator<<(ostream& _cout, const Date& d);
+
+	Date(int y = 1, int m = 1, int d = 1)
+		:_y(y), _m(m), _d(d)
+	{}
+private:
+	int _y;
+	int _m;
+	int _d;
+};
+//类外定义为普通函数，声明友元函数使得在类外可以访问到私有成员
+void operator<<(ostream& _cout, const Date& d)
+{
+	_cout << d._y << "-" << d._m << "-" << d._d << endl;
+}
+void test()
+{
+	Date d(2021, 3, 3);
+
+	//d << cout;    // 若 重载 operator<< 在类内定义
+
+	cout << d;      //定义友元之后输出方式
+}
+
+
+
 //class Date {
 //public:
-//	//友元函数的声明可以放在类内任何地方
-//	friend void operator<<(ostream& _cout, const Date& d);
-//
 //	Date(int y = 1, int m = 1, int d = 1)
 //		:_y(y), _m(m), _d(d)
 //	{}
+//	//类中成员函数，输出<<重载
+//	void operator<<(ostream& _cout)
+//	{
+//		_cout << _y << "-" << _m << "-" << _d << endl;
+//	}
 //private:
 //	int _y;
 //	int _m;
 //	int _d;
 //};
-//
-//void operator<<(ostream& _cout, const Date& d)
-//{
-//	_cout << d._y << "-" << d._m << "-" << d._d << endl;
-//}
 //void test()
 //{
 //	Date d(2021, 3, 3);
 //
-//	//d << cout;    // 若 重载 operator<< 在类内定义
-//
-//	cout << d;      //定义友元之后输出方式
+//	d.operator<<(cout);   //成员函数的调用方式
+//	//简写形式
+//	d << cout;
+//}
+
+
+
+
+//class A {
+//public:
+//	A(int a = 1)
+//		:_a(a)
+//	{}
+//private:
+//	int _a = 0;
+//	//此处声明的缺省值只有在没有其他值可选时才会用
+//	//仅限于 非static 成员
+//};
+//void test()
+//{
+//	A a;    // 此时 a 为 1
+//}
+
+//class A {
+//public:
+//	A(int a)
+//		:_a(a)
+//	{}
+//private:
+//	int _a = 0;
+//};
+//void test()
+//{
+//	A a(10);    // 此时 a 为 10
+//}
+
+//class A {
+//public:
+//	A()
+//	{}
+//private:
+//	int _a = 0;
+//};
+//void test()
+//{
+//	A a;    // 此时 a 为 0
+//}
+
+//class A {
+//public:
+//	A()
+//	{}
+//private:
+//	int _a;
+//};
+//void test()
+//{
+//	A a;    // 此时 a 为随机值
 //}
 
 
@@ -66,7 +147,7 @@ using namespace std;
 //{
 //	A aa(1); //默认构造
 //	A aobj = aa;     //拷贝构造
-//	A aobj2 = 20;    //会产生一个匿名对象
+//	A aobj2 = 20;    //会产生一个匿名对象，在调用拷贝构造
 //}
 
 
@@ -110,6 +191,7 @@ using namespace std;
 //		_hour = t._hour;
 //		_minute = t._minute;
 //		_second = t._second;
+//		return *this;
 //	}
 //private:
 //	int _hour;
@@ -137,10 +219,16 @@ using namespace std;
 //
 //	const int _hour;
 //
-//	Time _time;   //自定义成员没有默认构造，必须显示初始化
+//	Time _time;   //自定义成员没有默认构造，必须显式初始化
 //	// 若 _time 存在全缺省构造/默认构造则不需要显式初始化
 //};
 //void test()
 //{
 //	Date d(2021, 3, 3);
+//}
+
+//int main()
+//{
+//	test();
+//	return 0;
 //}
