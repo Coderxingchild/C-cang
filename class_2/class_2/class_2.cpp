861e78030d3d355d4b26d39a9250f2f1f74d8f20
Coderxingchild <1272209351@qq.com>
2021-03-08 17:54:02 +0800

    友元类 友元函数

---
+++ class_2/class_2/class_2.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+
+using namespace std;
+
+//友元类
+
+class Date {
+public:
+	Date(int year=1,int month=1,int day=1)
+		:_year(year),_month(month),_day(day)   //初始化列表
+	{}
+	void SetTimeOfDate(int hour, int minute, int second)
+	{   //设置时间
+		_time._hour = hour;
+		_time._minute = minute;
+		_time._second = second;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+	// 自定义类型
+	Time _time;
+};
+
+class Time {
+	friend class Date;   //友元类
+public:
+	Time(int hour = 1, int minute = 1, int second = 1) :_hour(hour), _minute(minute), _second(second)
+	{}
+	void fun()
+	{
+		cout << _hour << ":" << _minute << ":" << _second << endl;
+	}
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+
+
+
+
+
+
+
+
+
+//class Date {
+//	//声明友元函数
+//	friend ostream& operator<<(ostream& _cout, const Date& d);
+//	friend istream& operator>>(istream& _cin, Date& d);
+//public:
+//	Date(int y = 1, int m = 1, int d = 1)
+//		:_y(y),_m(m),_d(d)
+//	{}
+//private:
+//	int _y;
+//	int _m;
+//	int _d;
+//};
+////输出流作为返回值支持连续输出
+//ostream& operator<<(ostream& _cout, const Date& d)
+//{
+//	_cout << d._y << "-" << d._m << "-" << d._d;
+//	return _cout;
+//}
+//istream& operator>>(istream& _cin,Date& d)
+//{
+//	_cin >> d._y >> d._m >> d._d;
+//	return _cin;
+//}
+//
+//void test()
+//{
+//	Date d(2021, 3, 8);
+//	//输出
+//	cout << d;     //简写
+//	operator<<(cout, d);    //完整形式
+//
+//	//连续输出----友元函数返回值必须为 ostream&
+//	cout << d << endl;
+//	operator<<(cout, d) << endl;
+//
+//	cout << d << d;
+//	operator<<(operator<<(cout, d), d);
+//	cout.operator<<(endl); //成员函数
+//
+//
+//	//输入
+//	Date d(2021, 3, 8);
+//	cin >> d;
+//	//连续输入
+//	cin >> d >> d >> d;
+//}
+
+
+int main()
+{
+	test();
+	return 0;
+}
